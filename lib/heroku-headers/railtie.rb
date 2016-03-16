2760d8f4754d5803dccd9d628648cc1087bc71e1
David Backeus <duztdruid@gmail.com>
2016-03-16 16:53:22 +0100

    Only add middleware if ENV vars are available

--- lib/heroku-headers/railtie.rb
+++ lib/heroku-headers/railtie.rb
@@ -4,7 +4,11 @@ require "heroku-headers"
 module HerokuHeaders
   class Railtie < ::Rails::Railtie
     initializer "heroku-headers.middleware.rails" do |app|
-      app.config.middleware.use HerokuHeaders::Middleware
+      if ENV["HEROKU_RELEASE_VERSION"] && ENV["HEROKU_SLUG_COMMIT"]
+        app.config.middleware.use HerokuHeaders::Middleware
+      else
+        Rails.logger.debug "Skipping heroku-headers middleware since Heroku ENV variables not available"
+      end
     end
   end
 end
